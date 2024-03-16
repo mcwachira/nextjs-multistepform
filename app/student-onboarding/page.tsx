9bf901d0f8ab6856aa185a930717f1d9d0b144d0
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-03-16 18:11:24 +0300

    Made changes to the form to make ir work

--- app/student-onboarding/page.tsx
+++ app/student-onboarding/page.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import Steps from "@/components/MutiStepForm/Steps";
-import StepForm from "@/components/MutiStepForm/StepForm";
+import StepForm from "@/components/MultiStepForm/StepForm";
+import Steps from "@/components/MultiStepForm/Steps";
+import React from "react";
 
-function StudentOnboardingPage() {
-
+export default function StudentOnboardingPage() {
     const steps = [
         {
             number: 1,
@@ -11,7 +10,7 @@ function StudentOnboardingPage() {
         },
         {
             number: 2,
-            title: "Education Background",
+            title: "Education",
         },
         {
             number: 3,
@@ -19,35 +18,27 @@ function StudentOnboardingPage() {
         },
         {
             number: 4,
-            title: "Preferred Programing Languages",
+            title: "Preferred Programming Languages",
         },
         {
             number: 5,
-            title: "Technical Skills Assessments",
+            title: "Technical Skills Assessment",
         },
         {
             number: 6,
             title: "Submit and Confirmation",
         },
-
     ];
     return (
         <div className="bg-blue-50 min-h-screen p-4">
-            <div className="mx-auto w-full max-w-5xl bg-white border border-gray-200 rounded-lg shadow sm:p-4 md:p-6 dark:bg-gray-800 dark:border-gray-700 grid grid-cols-12 gap-4 min-h-screen">
-            {/*    steps*/}
-
-                <Steps steps={steps}/>
-            {/*    <Steps step={steps}/> */}
-            {/*    Form*/}
-
+            <div className="mx-auto w-full max-w-5xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-4 md:p-6 dark:bg-gray-800 dark:border-gray-700 grid grid-cols-12 gap-4 min-h-screen">
+                {/* Steps */}
+                <Steps steps={steps} />
+                {/* Form */}
                 <div className="rounded-lg col-span-full md:col-span-8">
-
-                    <StepForm/>
-                    {/*   Step Form*/}
+                    <StepForm />
                 </div>
             </div>
         </div>
     );
 }
-
-export default StudentOnboardingPage;
